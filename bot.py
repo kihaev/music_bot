e50aab0ce8e08cae6116282aa05aa445995b0f49
Yura Kikhaiev <50831818+kihaev@users.noreply.github.com>
2019-08-19 15:18:50 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,6 @@ elif mode == "prod":
     def run(updater):
         PORT = int(os.environ.get("PORT", "8443"))
         HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-        # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
         updater.start_webhook(listen="0.0.0.0",
                               port=PORT,
                               url_path=TOKEN)
